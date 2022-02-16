38bc8dffd78a38552ebe6781c38d801356ee0e96
StackSpot Admin <admin@stackspot.com>
2022-02-16 13:39:03 +0000

    Update public repository

--- templates/app/build.gradle.kts
+++ templates/app/build.gradle.kts
@@ -3,3 +3,5 @@ val beagleVersion: String by project
 dependencies {
     implementation("br.com.zup.beagle:beagle-spring-starter:$beagleVersion")
 }
+
+apply(from = "design-system.gradle.kts")
--- templates/app/design-system.gradle.kts
+++ templates/app/design-system.gradle.kts
@@ -1,11 +1,26 @@
 import org.apache.tools.ant.taskdefs.condition.Os
 
-val dsSchemaTask = task<Exec>("dsSchema") {
-    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
-        commandLine("./run-ds-shema.bat")
-    } else {
-        commandLine("./run-ds-shema.sh")
+task("dsSchema") {
+    description = "Runs DS Schema to generate DS Schema Components."
+    group = "design system schema"
+    doLast {
+        logger.info("Running Design System Schema...")
+        logger.info("Step 1 - Fetching json schemas from repository")
+        val tempDir = mkdir("temp")
+        exec {
+            workingDir(tempDir)
+            commandLine("git", "clone", "--depth", "1", "{{inputs.url}}")
+        }
+        logger.info("Step 2 - Executing ds-schema")
+        val dsSchemaCommand = "npx -q -p @zup-it/ds-schema-cli -p @zup-it/ds-schema-plugins -c ds-schema"
+        exec {
+            if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+                commandLine("cmd", "/c", dsSchemaCommand)
+            } else {
+                commandLine("sh", "-c", dsSchemaCommand)
+            }
+        }
+        tempDir.deleteRecursively()
+        logger.info("Finished Design System Schema")
     }
 }
-dsSchemaTask.setProperty(Task.TASK_DESCRIPTION, "Runs DS Schema to generate DS Schema Components.")
-dsSchemaTask.setProperty(Task.TASK_GROUP, "design system schema")
