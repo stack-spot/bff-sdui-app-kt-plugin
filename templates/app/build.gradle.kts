6af71a3eba21cc801bfbab3a8831bb4f3b8766e9
StackSpot Admin <admin@stackspot.com>
2022-02-22 12:48:31 +0000

    Update public repository

--- templates/app/build.gradle.kts
+++ templates/app/build.gradle.kts
@@ -4,4 +4,6 @@ dependencies {
     implementation("br.com.zup.beagle:beagle-spring-starter:$beagleVersion")
 }
 
+{% if inputs.configure_design_system %}
 apply(from = "design-system.gradle.kts")
+{% endif %}
